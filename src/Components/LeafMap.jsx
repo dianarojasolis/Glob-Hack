78d046f3bbcc293ea51f2fe313812f5c63b7468f
tamaramunoz <tami.skt@gmail.com>
2020-05-30 13:49:45 -0400

    geolocation working

--- src/Components/LeafMap.jsx
+++ src/Components/LeafMap.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Mapa.css'
 
 import MapContainer from './MapContainer'
-// import useGeolocation from 'react-hook-geolocation'
+import useGeolocation from 'react-hook-geolocation'
 
 const LeafMap = () => {
 
+    const geolocation = useGeolocation()
+    const [medicalStaff, setMedicalStaff] = useState([])
+
+
+    const latlng = { lat: geolocation.latitude, lng: geolocation.longitude };
+    let center = latlng;
+    let zoom = 16;
+
     return (
         <div>
-            <MapContainer />
+            <MapContainer 
+                center={center}
+                zoom={zoom}
+            
+            />
         </div>
     )
 }
--- src/Components/MapContainer.jsx
+++ src/Components/MapContainer.jsx
@@ -1,23 +1,30 @@
 import React, { Fragment } from 'react'
-import { Map, TileLayer } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
-const MapContainer = () => {
+
+const MapContainer = (props) => {
 
     const UrlMap = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
     
-    const center= [-33.4163883, -70.660074]
+    // const center= [-33.4163883, -70.660074]
 
     return (
         <Fragment>
             <Map
-                center={center}
-                zoom={16}
+                center={props.center}
+                zoom={props.zoom}
             >
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url={UrlMap}
                 />
+
+                <Marker position={props.center}>
+                    <Popup>
+                        Esta es mi ubicación actual
+                    </Popup>
+                </Marker>
             </Map>
 
         </Fragment>
