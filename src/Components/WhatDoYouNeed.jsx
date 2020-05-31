638a0c0077c6e60eef3e502b4bc272cf5ec48ffa
tamaramunoz <tami.skt@gmail.com>
2020-05-30 21:57:17 -0400

    form in component whatdoyouneed

--- src/Components/WhatDoYouNeed.jsx
+++ src/Components/WhatDoYouNeed.jsx
@@ -1,45 +1,52 @@
 import React from 'react'
+import uploadIcon from '../img/upload.png'
 
 
 const WhatDoYouNeed = () => {
+
     return (
         <div>
-        
+
             <h2>¿Qué Necesitas?</h2>
 
             <input type="text" placeholder="Tu ubicación" disabled />
 
-            <div>   
-                    <label htmlFor="">Tratamiento o examen a realizar</label>
-                    <select name="" id="">
-                        <option value="select"></option>
-                        <option value="ecografia">Ecografía</option>
-                        <option value="examenOrina">Examen de orina</option>
-                        <option value="examenSangre">Examen de sangre</option>
-                        <option value="inyeccionNeurobionta">Inyección Neurobionta</option>
-                        <option value="revisionDermatologica">Revisión Dermatológica</option>
-                        <option value="taco">TACO</option>
-                        <option value="endovenoso">Tratamiento endovenoso</option>
-                        <option value="psicologica">Visita Psicológica</option>
-                        <option value="psiquiatrica">Visita Psiquiátrica</option>
-                        <option value="reumatologica">Visita Reumatológica</option>
-                    </select>
-                </div>
+            <div>
+                <label htmlFor="">Tratamiento o examen a realizar</label>
+                <select name="" id="">
+                    <option value="select"></option>
+                    <option value="ecografia">Ecografía</option>
+                    <option value="examenOrina">Examen de orina</option>
+                    <option value="examenSangre">Examen de sangre</option>
+                    <option value="inyeccionNeurobionta">Inyección Neurobionta</option>
+                    <option value="revisionDermatologica">Revisión Dermatológica</option>
+                    <option value="taco">TACO</option>
+                    <option value="endovenoso">Tratamiento endovenoso</option>
+                    <option value="psicologica">Visita Psicológica</option>
+                    <option value="psiquiatrica">Visita Psiquiátrica</option>
+                    <option value="reumatologica">Visita Reumatológica</option>
+                </select>
+            </div>
 
-                <div>
-                    <label htmlFor="">Subir orden médica</label>
+            <div>
+                <label htmlFor="">Subir orden médica</label>
+                <input
+                    type="file"
+                    className="custom-file-input"
+                    id="inputGroupFile01"
+                />
+                <img src={uploadIcon} alt="subir imagen"/>
+            </div>
 
-                </div>
-
-                <div>
-                    <label htmlFor="">Para cuando lo necesitas</label>
-                    <select>
-                        <option value="select"></option>
-                        <option value="">Lo antes posible</option>
-                        <option value="">Durante el día</option>
-                        <option value="">Agendar cita</option>
-                    </select>
-                </div>
+            <div>
+                <label htmlFor="">Para cuando lo necesitas</label>
+                <select>
+                    <option value="select"></option>
+                    <option value="">Lo antes posible</option>
+                    <option value="">Durante el día</option>
+                    <option value="">Agendar cita</option>
+                </select>
+            </div>
         </div>
     )
 }
